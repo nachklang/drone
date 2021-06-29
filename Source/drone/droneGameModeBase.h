4f43f63fc3f040092c66bc3b824a256687808756
Aleksey Myasnikov <myasnikov.alexej@gmail.com>
2021-06-29 12:50:21 +0300

    Initial commit

---
+++ Source/drone/droneGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "droneGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DRONE_API AdroneGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
